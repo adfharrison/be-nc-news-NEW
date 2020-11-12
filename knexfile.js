b1a9a61f9397c234603374a8accfebf060052d53
adfharrsion <68917507+adfharrsion@users.noreply.github.com>
2020-11-12 12:18:59 +0000

    made changes for hosting

--- knexfile.js
+++ knexfile.js
@@ -1,28 +1,37 @@
-const ENV = process.env.NODE_ENV || "development";
+const { DB_URL } = process.env;
+const ENV = process.env.NODE_ENV || 'development';
 
 const baseConfig = {
-  client: "pg",
+  client: 'pg',
   migrations: {
-    directory: "./db/migrations",
+    directory: './db/migrations',
   },
   seeds: {
-    directory: "./db/seeds",
+    directory: './db/seeds',
   },
 };
 
 const customConfig = {
   development: {
     connection: {
-      database: "nc_news",
-      username: "francisco",
-      password: "@Pezespada12",
+      database: 'nc_news',
+      username: 'francisco',
+      password: '@Pezespada12',
     },
   },
   test: {
     connection: {
-      database: "nc_news_test",
-      username: "francisco",
-      password: "@Pezespada12",
+      database: 'nc_news_test',
+      username: 'francisco',
+      password: '@Pezespada12',
+    },
+  },
+  production: {
+    connection: {
+      connectionString: DB_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      },
     },
   },
 };
