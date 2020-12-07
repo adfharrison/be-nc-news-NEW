738604847f964c18e454d3c91e1af0d328bec1a5
adfharrsion <68917507+adfharrsion@users.noreply.github.com>
2020-12-07 15:05:41 +0000

    added CORS'\n

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const {
 const app = express();
 const apiRouter = require('./routers/apiRouter');
 
+const cors = require('cors');
+
+app.use(cors());
+
 app.use(express.json());
 app.use('/api', apiRouter);
 
